2166c7f3797965c3e17a962d19617d543ffb4b7d
henrique <hickm018@gmail.com>
2022-03-04 05:45:51 -0300

    Organiza idéais e estrutura o projeto

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Header from "../Components/Header";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Header />
+      <h1>Quem ousou conquistar e saiu pra lutar, chega mais longe!</h1>
+      <h3>Charlie Brown Jr</h3>
+      <p>
+        TEXTO SOBRE MIM! 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Voluptatum blanditiis, aspernatur quibusdam adipisci 
+        culpa nam, praesentium amet expedita corporis repellat 
+        magni minima obcaecati ex exercitationem sit! Quam sequi culpa laborum?
+      </p>
+      <p>Algumas imagens(abstrações, emojis, logos techs?)</p>
+    </div>
+  )
+}
---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,10 @@
+import Header from "../Components/Header";
+
+export default function ProjectsPage() {
+  return (
+    <div>
+      <Header />
+      <p>CARD DE PROJETOS(COMPONENTE SEPARADO)</p>
+    </div>
+  )
+}
