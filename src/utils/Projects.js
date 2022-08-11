2272b82826f462aa3bd1901edae42a79d5c25f94
henrique <hickm018@gmail.com>
2022-08-11 00:44:24 -0300

    feat: create array of projects

---
+++ src/utils/Projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    name: 'App de Receitas: Drinks and Foods',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: 'https://www.portalgsti.com.br/media/uploads/marcomascarenhas/javascript.png',
+    gitHub: 'https://www.google.com',
+    techs: 'Redux, React, Node, Express, MongoDB',
+  },
+  {
+    name: 'App de delivery: Embreagados',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: 'https://www.portalgsti.com.br/media/uploads/marcomascarenhas/javascript.png',
+    gitHub: 'https://www.google.com',
+    techs: 'Redux, React, Node, Express, MongoDB',
+  },
+  {
+    name: 'Backend: API de jogos de campeonato',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: 'https://www.portalgsti.com.br/media/uploads/marcomascarenhas/javascript.png',
+    gitHub: 'https://www.google.com',
+    techs: 'Redux, React, Node, Express, MongoDB',
+  },
+  {
+    name: 'Frontend: App de musicas',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: 'https://www.portalgsti.com.br/media/uploads/marcomascarenhas/javascript.png',
+    gitHub: 'https://www.google.com',
+    techs: 'Redux, React, Node, Express, MongoDB',
+  },
+];
+
+export default projects;
